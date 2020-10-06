fafb59df283987ed8074a8845c976b7b8b28f1b1
Andrew Westberg <andrew@swiftmako.com>
2020-10-06 08:26:24 -0700

    Final Tweaks to leader logs

--- src/main/kotlin/com/swiftmako/jormanager/monitors/BlockMonitor.kt
+++ src/main/kotlin/com/swiftmako/jormanager/monitors/BlockMonitor.kt
@@ -153,9 +153,8 @@ class BlockMonitor @Autowired constructor(
                 try {
                     // find the latest block we know of for sure from the repository
                     val chainTipSlotNumber = chainRepository.findSyncedTip()
-                    log.error("findSyncedTip: $chainTipSlotNumber")
                     val unvalidatedBlocks = blockRepository.findUnvalidatedBlocksOlderThan(chainTipSlotNumber - 180) // 3 minutes old
-                    log.error("unvalidatedBlocks size: ${unvalidatedBlocks.size}")
+                    log.debug("unvalidatedBlocks size: ${unvalidatedBlocks.size}")
                     unvalidatedBlocks.forEach { unvalidatedBlock ->
                         if (unvalidatedBlock.hash.isEmpty()) {
                             // nothing to validate. This block must have been missed
