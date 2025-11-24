a4798d18d633b24af881628a02e3ccfc7b1290c3
Andrew Westberg <andrewwestberg@gmail.com>
2025-11-24 19:24:46 +0000

    fix: set node version during build

--- vue/deploy.sh
+++ vue/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pushd $(dirname "$0")
+export NVM_DIR=$HOME/.nvm
+source $NVM_DIR/nvm.sh
+nvm use v16.17.0
 npm run build
 rsync -av --progress --delete dist/ ../src/main/resources/static
 popd
